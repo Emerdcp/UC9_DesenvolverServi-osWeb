f3327c2867782e023d414e509ddffa8b7b257c4d
Emerson Carvalho <emersoncarvalho@hotmail.com.br>
2026-03-24 20:18:59 -0300

    correção do clima

--- 2026.03.23_APITemperatura/server.js
+++ 2026.03.23_APITemperatura/server.js
@@ -1,5 +1,8 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
@@ -7,34 +10,46 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 async function buscarClima(cidade) {
-    const localRes = await fetch(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1`
-    );
-    const localData = await localRes.json();
+    try {
+        const localRes = await axios.get(
+            `https://geocoding-api.open-meteo.com/v1/search?name=${cidade}&count=1`
+        );
 
-    if (!localData.results || localData.results.length === 0) return null;
+        const localData = localRes.data;
 
-    const local = localData.results[0];
+        if (!localData.results || localData.results.length === 0) {
+            return null;
+        }
 
-    const climaRes = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${local.latitude}&longitude=${local.longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,sunrise,sunset&timezone=America/Sao_Paulo`
-    );
+        const local = localData.results[0];
 
-    const clima = await climaRes.json();
+        const climaRes = await axios.get(
+            `https://api.open-meteo.com/v1/forecast?latitude=${local.latitude}&longitude=${local.longitude}&current=temperature_2m,wind_speed_10m,relative_humidity_2m&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=America/Sao_Paulo`
+        );
+        const clima = climaRes.data;
 
-    return { local, clima };
+        return { local, clima };
+
+    } catch (err) {
+        console.error("ERRO DETALHADO:", err.message);
+        throw err;
+    }
 }
 
 app.get("/api/clima", async (req, res) => {
     const cidade = req.query.cidade || "Americana";
 
-    const data = await buscarClima(cidade);
+    try {
+        const data = await buscarClima(cidade);
 
-    if (!data) {
-        return res.status(404).json({ error: "Cidade não encontrada" });
+        if (!data) {
+            return res.status(404).json({ error: "Cidade não encontrada" });
+        }
+
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: "Erro interno do servidor" });
     }
-
-    res.json(data);
 });
 
 app.listen(port, () => {
